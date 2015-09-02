1f323c164b5592954af012a475a811afac355821
Michael Clark <kovels@hotmail.com>
2015-09-02 16:48:31 -0400

    init

---
+++ SoftwareTestingDataGenerator/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace SoftwareTestingDataGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numMain = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.numMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numMain
+            // 
+            this.numMain.Increment = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numMain.Location = new System.Drawing.Point(12, 23);
+            this.numMain.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.numMain.Name = "numMain";
+            this.numMain.Size = new System.Drawing.Size(171, 20);
+            this.numMain.TabIndex = 1;
+            this.numMain.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(304, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Amount of students";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Save Location";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(125, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Go!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 95);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(304, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "example";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Name Your File";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(2, 166);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(322, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 192);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numMain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numMain;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ SoftwareTestingDataGenerator/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoftwareTestingDataGenerator
+{
+    public partial class Form1 : Form
+    {
+
+        List<string> _names = new List<string>{ "tom", "robert", "albert"};
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //Verify directory
+            if(!Directory.Exists(textBox1.Text))
+            {
+                if(MessageBox.Show("Your directory doesn't exist brah, create it?", "Hey Buddy", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    Directory.CreateDirectory(textBox1.Text);
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            string preprocess = textBox2.Text.Split(',')[0];
+
+            Regex rgx = new Regex("[^a-zA-Z0-9 -]");
+            string str = rgx.Replace(preprocess, "");
+            string fullFilePath = Path.Combine(textBox1.Text, str + ".txt");
+
+            if (File.Exists(fullFilePath))
+            {
+                if(MessageBox.Show("File already exists! Replace it?", "File Eixsts", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    File.Delete(fullFilePath);
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            button1.Enabled = false;
+            numMain.Enabled = false;
+
+            BackgroundWorker bw = new BackgroundWorker();
+
+            bw.DoWork += (object obj, DoWorkEventArgs ev) =>
+            {
+                progressBar1.Maximum = (int)numMain.Value;
+
+                List<string> toAdd = new List<string>();
+                for (int i = 0; i < numMain.Value; i++)
+                {
+                    string name = randomName();
+                    List<string> infoString = new List<string> { name, i.ToString(), name + "@gmail.com" };
+                    infoString.AddRange(randomScores());
+                    toAdd.Add(string.Join(",", infoString));
+
+                    if (i % 10000 == 0)
+                    {
+                        File.AppendAllLines(fullFilePath, toAdd.ToArray());
+                        progressBar1.Value = i;
+                        toAdd.Clear();
+                    }
+                }
+                //Get the strays
+                File.AppendAllLines(fullFilePath, toAdd.ToArray());
+            };
+
+            bw.RunWorkerCompleted += (object obj, RunWorkerCompletedEventArgs ev) =>
+            {
+                textBox1.Enabled = true;
+                textBox2.Enabled = true;
+                button1.Enabled = true;
+                numMain.Enabled = true;
+                progressBar1.Value = progressBar1.Maximum;
+
+                MessageBox.Show("DONE!");
+
+            };
+
+            bw.RunWorkerAsync();
+
+            //using (System.IO.StreamWriter file =
+            //new System.IO.StreamWriter(fullFilePath))
+            //{
+            //    foreach (string line in toAdd)
+            //    {
+            //        file.WriteLine(line);
+            //    }
+            //}
+        }
+
+        Random rand = new Random();
+        private string randomName()
+        {
+            
+            int random = rand.Next(_names.Count);
+            return _names[random];
+        }
+
+        private List<string> randomScores()
+        {
+            return new List<string> { rand.Next(4).ToString(), rand.Next(4).ToString(), rand.Next(4).ToString() };
+        }
+    }
+}
